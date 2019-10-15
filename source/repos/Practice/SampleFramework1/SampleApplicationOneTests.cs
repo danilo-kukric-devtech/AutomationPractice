823224e1fdf440900ceac3b8b0b99b424665cdad
Danilo Kukric <danilokukric988@gmail.com>
2019-10-15 11:32:45 +0200

    Sprint 3 and 4

--- source/repos/Practice/SampleFramework1/SampleApplicationOneTests.cs
+++ source/repos/Practice/SampleFramework1/SampleApplicationOneTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -12,28 +13,41 @@ namespace SampleFramework1
     public class SampleApplicationOneTests
     {
         private IWebDriver Driver { get; set; }
+        internal SampleAppPage SampleAppPage { get; private set; }
         public TestUser TheTestUser { get; private set; }
+        public TestUser EmergencyContactUser { get; set; }
 
         [TestMethod]
+        [Description("Fillout form")]
         public void Test1()
         {
-            var sampleApplicationPage = new SampleApplicationPage(Driver);
-            sampleApplicationPage.GoTo();
-           
-            var ultimateHomePage = sampleApplicationPage.FillOutFormAndSubmit(TheTestUser);
-            Assert.IsTrue(ultimateHomePage.IsVisible,"UltimateQA home page is not visible");
             
+            SetGenderTypes(Gender.Female, Gender.Female);
+            SampleAppPage.GoTo();
+            SampleAppPage.FillOutEmergencyContactForm(EmergencyContactUser);
+            var ultimateHomePage = SampleAppPage.FillOutFormAndSubmit(TheTestUser);
+            AssertPageVisisble(ultimateHomePage);
         }
         [TestMethod]
+        [Description("Negative test")]
         public void PretendTestNumber2()
         {
-            var sampleApplicationPage = new SampleApplicationPage(Driver);
-            sampleApplicationPage.GoTo();
-           
-            var ultimateHomePage = sampleApplicationPage.FillOutFormAndSubmit(TheTestUser);
-            Assert.IsFalse(!ultimateHomePage.IsVisible, "UltimateQA home page is not visible");
+            TheTestUser.GenderType = Gender.Female;
+            SampleAppPage.GoTo();
+            SampleAppPage.FillOutEmergencyContactForm(EmergencyContactUser);
+            var ultimateHomePage = SampleAppPage.FillOutFormAndSubmit(TheTestUser);
+            AssertPageVisisbleVariation2(ultimateHomePage);
         }
-     
+        [TestMethod]
+        [Description("Select other gender")]
+        public void Test3()
+        {
+            SetGenderTypes(Gender.Other, Gender.Other);
+            SampleAppPage.GoTo();
+            SampleAppPage.FillOutEmergencyContactForm(EmergencyContactUser);
+            var ultimateHomePage = SampleAppPage.FillOutFormAndSubmit(TheTestUser);
+            AssertPageVisisble(ultimateHomePage);
+        }
         private IWebDriver GetChromeDriver()
         {
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -43,9 +57,23 @@ namespace SampleFramework1
         public void SetupForEverySingleTestMethod()
         {
             Driver = GetChromeDriver();
+            SampleAppPage = new SampleAppPage(Driver);
             TheTestUser = new TestUser();
             TheTestUser.FirstName = "Danilo";
             TheTestUser.LastName = "Kukric";
+        }
+        private static void AssertPageVisisble(UltimateQAHomePage ultimateHomePage)
+        {
+            Assert.IsTrue(ultimateHomePage.IsVisible, "UltimateQA home page is not visible");
+        }
+        private void AssertPageVisisbleVariation2(UltimateQAHomePage ultimateHomePage)
+        {
+            Assert.IsFalse(!ultimateHomePage.IsVisible, "UltimateQA home page is not visible");
+        }
+        private void SetGenderTypes(Gender primaryContact, Gender emergencyContact)
+        {
+            TheTestUser.GenderType = primaryContact;
+            EmergencyContactUser.GenderType = emergencyContact;
         }
         [TestCleanup]
         public void CleanupAfterEveryTestMethod()
@@ -53,5 +81,6 @@ namespace SampleFramework1
             Driver.Close();
             Driver.Quit();
         }
+
     }
 }
--- source/repos/Practice/SampleFramework1/SampleApplicationPage.cs
+++ source/repos/Practice/SampleFramework1/SampleApplicationPage.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 
 namespace SampleFramework1
 {
-    internal class SampleApplicationPage : BaseSampeApplicationPage
+    internal class SampleAppPage : BaseSampeApplicationPage
     {
 
-        public SampleApplicationPage(IWebDriver driver): base(driver)
+        public SampleAppPage(IWebDriver driver): base(driver)
         {
         }
+
+        //Page Title
+        private string PageTitleSprint3 => "Sample Application Lifecycle - Sprint 3 | Ultimate QA";
+        //First Name Field
+        public IWebElement FirstNameField => Driver.FindElement(By.Name("firstname"));
+        //Submit button
+        public IWebElement SubmitButton => Driver.FindElement(By.XPath("//*[@type='submit']"));
+        //Last Name Field
+        public IWebElement LastNameField => Driver.FindElement(By.Name("lastname"));
+        //Female gender radio button
+        public IWebElement FemaleGenderRadioButton => Driver.FindElement(By.XPath("//input[@value='female']"));
+        //Other radio button
+        public IWebElement OtherGenderRadioButton => Driver.FindElement(By.XPath("//input[@value='other']"));
+        public IWebElement FemaleGenderRadioButtonForEmergencyContact => Driver.FindElement(By.Id("radio2-f"));
+        public IWebElement FirstNameFieldForEmergencyContact => Driver.FindElement(By.Id("f2"));
+        public IWebElement LastNameFieldForEmergencycontact => Driver.FindElement(By.Id("l2"));
 
         public bool IsVisible
         {
@@ -19,32 +36,74 @@ namespace SampleFramework1
             }
              internal set{ }
         }
+        public bool IsVisibleSprint3Page
+        {
+
+            get
+            {
+                return Driver.Title.Contains(PageTitleSprint3);
+
+            }
+            internal set { }
+        }
         //Page Title
         private string PageTitle => "Sample Application Lifecycle - Sprint 2 | Ultimate QA";
-        //First Name Field
-        public IWebElement FirstNameField => Driver.FindElement(By.Name("firstname"));
-        //Submit button
-        public IWebElement SubmitButton => Driver.FindElement(By.XPath("//*[@type='submit']"));
-        //Last Name Field
-        public IWebElement LastNameField => Driver.FindElement(By.Name("lastname"));
-        
 
+        internal void FillOutEmergencyContactForm(TestUser emergencyContactUser)
+        {
+            SetGenderForEmergencyContact(emergencyContactUser);
+            FirstNameFieldForEmergencyContact.SendKeys(emergencyContactUser.FirstName);
+            LastNameFieldForEmergencycontact.SendKeys(emergencyContactUser.LastName);
+        }
+
+        private void SetGenderForEmergencyContact(TestUser user)
+        {
+            switch (user.GenderType)
+            {
+                case Gender.Male:
+                    break;
+                case Gender.Female:
+                    FemaleGenderRadioButtonForEmergencyContact.Click();
+                    break;
+                case Gender.Other:
+                    OtherGenderRadioButton.Click();
+                    break;
+                default:
+                    break;
+            }
+        }
         public void GoTo()
         {
-           Driver.Navigate().GoToUrl("https://www.ultimateqa.com/sample-application-lifecycle-sprint-2/");
-           Assert.IsTrue(IsVisible, 
-               $"Sample application page is not visible. Expected => {PageTitle}." +
+           Driver.Navigate().GoToUrl("https://www.ultimateqa.com/sample-application-lifecycle-sprint-3/");
+           Assert.IsTrue(IsVisibleSprint3Page, 
+               $"Sample application page is not visible. Expected => {PageTitleSprint3}." +
                $"Actual =>{Driver.Title}");
         }
 
         public UltimateQAHomePage FillOutFormAndSubmit(TestUser user)
         {
+            SetGender(user);
             FirstNameField.SendKeys(user.FirstName);
             LastNameField.SendKeys(user.LastName);
             SubmitButton.Click();
           return new UltimateQAHomePage(Driver);
         }
-
+        private void SetGender(TestUser user)
+        {
+            switch (user.GenderType)
+            {
+                case Gender.Male:
+                    break;
+                case Gender.Female:
+                    FemaleGenderRadioButton.Click();
+                    break;
+                case Gender.Other:
+                    OtherGenderRadioButton.Click();
+                    break;
+                default:
+                    break;
+            }
+        }
         public UltimateQAHomePage FillOutLastName(string value)
         {
            LastNameField.SendKeys(value);
